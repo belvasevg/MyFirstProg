769a6a574bcd7bbf795a3d1a3faa91321d3c4c3d
belvasevg <belvasevg@yandex.ru>
2024-06-03 18:39:58 +0300

    В новую ветку добавлена функция вывода параметров человека.

---
+++ CA_git2_0306/func_Person.cpp
@@ -0,0 +1,9 @@
+#include "func_Person.h"
+
+void print_pars_person(Person p)
+{
+	cout << "Имя человека: " << p.name << endl;
+	cout << "Фамилия человека: " << p.surname << endl;
+	cout << "Возраст человека: " << p.age << endl;
+	cout << "Вес человека: " << p.weight << endl;
+}
